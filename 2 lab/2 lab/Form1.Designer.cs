c000f253cf0e05d51fcde4dae9291612f7716b11
ThereFox <59343016+ThereFox@users.noreply.github.com>
2024-02-29 22:10:38 +0300

    Add files via upload

---
+++ 2 lab/2 lab/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace _2_lab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.pictureBox13 = new System.Windows.Forms.PictureBox();
+            this.pictureBox14 = new System.Windows.Forms.PictureBox();
+            this.pictureBox15 = new System.Windows.Forms.PictureBox();
+            this.pictureBox16 = new System.Windows.Forms.PictureBox();
+            this.pictureBox17 = new System.Windows.Forms.PictureBox();
+            this.pictureBox18 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox15)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox16)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox17)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox18)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(0)))), ((int)(((byte)(51)))));
+            this.pictureBox12.Image = global::_2_lab.Properties.Resources._removal_ai__bae450db_5e69_4ca1_826d_f98356a1917d_stars;
+            this.pictureBox12.Location = new System.Drawing.Point(989, 344);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox12.TabIndex = 13;
+            this.pictureBox12.TabStop = false;
+            // 
+            // pictureBox13
+            // 
+            this.pictureBox13.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(0)))), ((int)(((byte)(51)))));
+            this.pictureBox13.Image = global::_2_lab.Properties.Resources._removal_ai__bae450db_5e69_4ca1_826d_f98356a1917d_stars;
+            this.pictureBox13.Location = new System.Drawing.Point(969, 47);
+            this.pictureBox13.Name = "pictureBox13";
+            this.pictureBox13.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox13.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox13.TabIndex = 14;
+            this.pictureBox13.TabStop = false;
+            // 
+            // pictureBox14
+            // 
+            this.pictureBox14.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(0)))), ((int)(((byte)(51)))));
+            this.pictureBox14.Image = global::_2_lab.Properties.Resources._removal_ai__bae450db_5e69_4ca1_826d_f98356a1917d_stars;
+            this.pictureBox14.Location = new System.Drawing.Point(827, 219);
+            this.pictureBox14.Name = "pictureBox14";
+            this.pictureBox14.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox14.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox14.TabIndex = 15;
+            this.pictureBox14.TabStop = false;
+            // 
+            // pictureBox15
+            // 
+            this.pictureBox15.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(0)))), ((int)(((byte)(51)))));
+            this.pictureBox15.Image = global::_2_lab.Properties.Resources._removal_ai__bae450db_5e69_4ca1_826d_f98356a1917d_stars;
+            this.pictureBox15.Location = new System.Drawing.Point(748, 32);
+            this.pictureBox15.Name = "pictureBox15";
+            this.pictureBox15.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox15.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox15.TabIndex = 16;
+            this.pictureBox15.TabStop = false;
+            // 
+            // pictureBox16
+            // 
+            this.pictureBox16.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(0)))), ((int)(((byte)(51)))));
+            this.pictureBox16.Image = global::_2_lab.Properties.Resources._removal_ai__bae450db_5e69_4ca1_826d_f98356a1917d_stars;
+            this.pictureBox16.Location = new System.Drawing.Point(589, 93);
+            this.pictureBox16.Name = "pictureBox16";
+            this.pictureBox16.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox16.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox16.TabIndex = 17;
+            this.pictureBox16.TabStop = false;
+            // 
+            // pictureBox17
+            // 
+            this.pictureBox17.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(0)))), ((int)(((byte)(51)))));
+            this.pictureBox17.Image = global::_2_lab.Properties.Resources._removal_ai__bae450db_5e69_4ca1_826d_f98356a1917d_stars;
+            this.pictureBox17.Location = new System.Drawing.Point(97, 32);
+            this.pictureBox17.Name = "pictureBox17";
+            this.pictureBox17.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox17.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox17.TabIndex = 18;
+            this.pictureBox17.TabStop = false;
+            // 
+            // pictureBox18
+            // 
+            this.pictureBox18.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(0)))), ((int)(((byte)(51)))));
+            this.pictureBox18.Image = global::_2_lab.Properties.Resources._removal_ai__bae450db_5e69_4ca1_826d_f98356a1917d_stars;
+            this.pictureBox18.Location = new System.Drawing.Point(313, 32);
+            this.pictureBox18.Name = "pictureBox18";
+            this.pictureBox18.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox18.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox18.TabIndex = 19;
+            this.pictureBox18.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(0)))), ((int)(((byte)(51)))));
+            this.pictureBox5.Image = global::_2_lab.Properties.Resources._removal_ai__bae450db_5e69_4ca1_826d_f98356a1917d_stars;
+            this.pictureBox5.Location = new System.Drawing.Point(42, 219);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(0)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(1184, 662);
+            this.Controls.Add(this.pictureBox18);
+            this.Controls.Add(this.pictureBox17);
+            this.Controls.Add(this.pictureBox16);
+            this.Controls.Add(this.pictureBox15);
+            this.Controls.Add(this.pictureBox14);
+            this.Controls.Add(this.pictureBox13);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.pictureBox5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox15)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox16)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox17)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox18)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.PictureBox pictureBox13;
+        private System.Windows.Forms.PictureBox pictureBox14;
+        private System.Windows.Forms.PictureBox pictureBox15;
+        private System.Windows.Forms.PictureBox pictureBox16;
+        private System.Windows.Forms.PictureBox pictureBox17;
+        private System.Windows.Forms.PictureBox pictureBox18;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
+
---
+++ 2 lab/2 lab/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System.Diagnostics.Tracing;
+using System.Runtime.CompilerServices;
+using System.Globalization;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2_lab
+{
+    public partial class Form1 : Form
+    {
+        private int x = 500;
+        private int y = 700;
+        private int flag = 0,
+        private  int q = 0;
+
+        private Pen _pen = new  Pen(Color.Yellow, 5);
+        private Graphics _graph;
+
+        public Form1()
+        {
+            _graph = CreateGraphics();
+            InitializeComponent();
+            
+        }
+        
+        private async void Animate()
+        {
+            while(true)
+            {
+                if(y <= 220)
+                {
+                    g.DrawLine(pen, x, y, x, y - 20);
+                    y -= 22;
+                }
+                for (var i = 0; i < 2; i++)
+                {
+                    await Explose();
+                    await Task.Delay(100);
+                    await PhaseOne();
+                    await Task.Delay(100);
+                    await PhaseTwo();
+                    await Task.Delay(100);
+                }
+
+            }
+        }
+
+        private async Task Explose()
+        {
+            _graph.Clear(BackColor);
+            _graph.DrawEllipse(pen, 499, 337, 2, 2);
+        }
+
+        private async Task PhaseOne()
+        {
+            _graph.DrawLine(pen, 500, 330, 500, 300);
+            await Task.Delay(2);
+            _graph.DrawLine(pen, 490, 330, 450, 300);
+            await Task.Delay(2);
+            _graph.DrawLine(pen, 510, 330, 550, 300);
+            await Task.Delay(2);
+            _graph.DrawLine(pen, 490, 345, 450, 370);
+            await Task.Delay(2);
+            _graph.DrawLine(pen, 510, 345, 550, 370);
+            await Task.Delay(2);
+        }
+        private async Task PhaseTwo()
+        {
+            _graph.DrawLine(pen, 500, 295, 500, 255);
+            await Task.Delay(2);
+            _graph.DrawLine(pen, 440, 295, 400, 265);
+            await Task.Delay(2);
+            _graph.DrawLine(pen, 560, 295, 600, 265);
+            await Task.Delay(2);
+            _graph.DrawLine(pen, 440, 375, 400, 400);
+            await Task.Delay(2);
+            _graph.DrawLine(pen, 560, 375, 600, 400);
+            await Task.Delay(2);
+        }
+
+        private async Task SycleEnd()
+        {
+            g.Clear(BackColor);
+            y = 700;
+        }
+    }
+}
